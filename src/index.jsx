ceaf6d32adbb06c546e41ab0f8e906559e73f873
Sada <shukurova.sada@gmail.com>
2024-03-27 16:05:21 +0400

    core concepts

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./App.jsx";
+import "./index.css";
+import { StrictMode } from "react";
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
